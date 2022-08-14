40036a1f8f4e8c7a7471e9194c2d7875df8875b6
Gabriel Castilho <gabrielmcastilho@gmail.com>
2022-08-14 20:31:25 -0300

    seeder add produto

--- backend/models/product.js
+++ backend/models/product.js
@@ -44,7 +44,7 @@ const productSchema = new mongoose.Schema({
                 'Accessories',
                 'Headphones',
                 'Food',
-                "Books",
+                'Books',
                 'Clothes/Shoes',
                 'Beauty/Health',
                 'Sports',
