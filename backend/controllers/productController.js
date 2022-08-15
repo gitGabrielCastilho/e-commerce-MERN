4fce90dc34c77e40a1f2615fdbc94f46e59c82bc
Gabriel Castilho <gabrielmcastilho@gmail.com>
2022-08-15 19:20:33 -0300

    crud finalizado

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -20,3 +20,61 @@ exports.getProducts = async (req, res, next) => {
         products
     })
 }
+
+exports.getSingleProduct = async (req, res, next) => {
+
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found'
+            })
+        }
+    
+
+    res.status(200).json({
+            success: true,
+            product
+            })
+}
+
+exports.updateProduct = async (req, res, next) => {
+    
+    let product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body,{
+        new: true,
+        runValidators: true,
+    });
+
+    res.status(200).json({
+    success: true,
+    product
+    })
+
+}
+
+exports.deleteProduct = async (req, res, next) => {
+
+    let product = await Product.findById(req.params.id);
+    
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found'
+            })
+        }
+    await product.deleteOne();
+
+    res.status(200).json({
+        success: true,
+        message: 'Product deleted successfully' })
+
+}
