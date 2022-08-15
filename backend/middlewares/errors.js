3e8df78d05cbfdae60ac65b4f1e2b99c68848b7b
Gabriel Castilho <gabrielmcastilho@gmail.com>
2022-08-15 20:54:41 -0300

    error handler production/development

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -2,10 +2,23 @@ const ErrorHandler = require('../utils/errorHandler');
 
 module.exports = (err, req, res, next) => {
     err.statusCode = err.status || 500;
-    err.message = err.message || 'Internal Server Error';
+    
+    if(process.env.NODE_ENV === 'DEVELOPMENT') {
+        res.status(err.statusCode).json({
+            success: false,
+            error: err,
+            errMessage: err.message,
+            stack: err.stack
+        })
+    }
 
-    res.status(err.statusCode).json({
+    if(process.env.NODE_ENV === 'PRODUCTION') {
+        let error = {...err}
+        error.message = err.message
+
+
+    res.status(error.statusCode).json({
         success: false,
-        error: err
+        message: error.message || 'Internal Server Error'
     })
-}
+}}
