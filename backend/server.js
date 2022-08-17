da8fd79d09d1c78436e461cbe2ee930da3fc1282
Gabriel Castilho <gabrielmcastilho@gmail.com>
2022-08-17 20:33:59 -0300

    error handlers

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,27 @@ const dotenv = require('dotenv');
 
 dotenv.config({path: 'backend/config/config.env'})
 
+process.on('uncaughtException',err => {
+    conso.log(`Error: ${err.stack}`);
+    console.log('shutting down the server due to uncaughtException');
+    server.close(() => { 
+        process.exit(1);
+    })
+
+})
+
 
 connectDatabase();
 
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
     console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`)
 })
+
+process.on('unhandledRejection', err => {
+    console.log(`ERROR: ${err.message}`);
+    console.log('shutting down the server due to Unhandled Promise rejection');
+    server.close(() => { 
+        process.exit(1);
+    })
+}) 
