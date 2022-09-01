debf60351b06c56b81167b2f93e3f44ad3f94ce3
Gabriel <gabrielmcastilho@gmail.com>
2022-09-01 10:47:09 -0300

    rotas login, logut, admin

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,13 @@
 const express  = require('express');
 const app = express();
 
+
+const cookieParser = require('cookie-parser')
+
 const errorMiddleware = require('./middlewares/errors')
 
 app.use(express.json());
-
+app.use(cookieParser());
 
 const products = require('./routes/product');
 const auth = require('./routes/auth');
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -3,13 +3,17 @@ const router = express.Router();
 
 const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/productController')
 
+
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth')
+
+
 router.route('/product').get(getProducts);
 router.route('/product/:id').get(getSingleProduct);
 
-router.route('/admin/product/new').post(newProduct);
+router.route('/admin/product/new').post(isAuthenticatedUser, authorizeRoles('admin'), newProduct);
 
-router.route('/admin/product/:id').put(updateProduct);
+router.route('/admin/product/:id').put(isAuthenticatedUser, authorizeRoles('admin'), updateProduct);
 
-router.route('/admin/product/:id').delete(deleteProduct);
+router.route('/admin/product/:id').delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProduct);
 
 module.exports = router;
