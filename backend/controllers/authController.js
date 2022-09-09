5b2b57705e99ef12c2f08255aeea9be71758743a
Gabriel <gabrielmcastilho@gmail.com>
2022-09-09 10:57:11 -0300

    edit e delete user

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -182,3 +182,67 @@ exports.updateProfile =  catchAsyncError(async (req,res,next) =>{
     })
 
 })
+
+
+exports.allUsers = catchAsyncError(async (req, res, next) =>{
+
+    const users = await User.find();
+
+    res.status(200).json({
+        success: true,
+        users
+    })
+
+})
+
+exports.getUserDetails = catchAsyncError(async (req, res, next) =>{
+
+    const user = await User.findById(req.params.id);
+
+    if(!user) {
+        return next(new ErrorHandler(`User does not found with id: ${req.params.id}`))
+    }
+
+    res.status(200).json({
+        success: true,
+        user
+    })
+
+})
+
+exports.updateUser =  catchAsyncError(async (req,res,next) =>{
+
+    const newUserData = {
+        name: req.body.name,
+        email: req.body.email,
+        role: req.body.role
+    }
+
+    const user = await User.findByIdAndUpdate(req.params.id, newUserData, {
+        
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    })
+
+    res.status(200).json({
+        success: true
+    })
+
+})
+
+exports.deleteUser = catchAsyncError(async (req, res, next) =>{
+
+    const user = await User.findById(req.params.id);
+
+    if(!user) {
+        return next(new ErrorHandler(`User does not found with id: ${req.params.id}`))
+    }
+
+    await user.remove();
+
+    res.status(200).json({
+        success: true,
+    })
+
+})
