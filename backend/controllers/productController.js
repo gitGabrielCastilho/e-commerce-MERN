1d112200a7d6f17debc6b4b29d1549bac0b29bd8
Gabriel <gabrielmcastilho@gmail.com>
2022-09-30 11:48:56 -0300

    create, update, delete review

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -87,3 +87,77 @@ exports.deleteProduct = catchAsyncErrors (async (req, res, next) => {
         message: 'Product deleted successfully' })
 
 })
+
+exports.createProductReview = catchAsyncErrors( async (req, res, next) =>{
+
+    const {rating, comment, productId} = req.body;
+
+    const review = {
+        user: req.user._id,
+        name: req.user.name,
+        rating: Number(rating),
+        comment
+    }
+
+    const product = await Product.findById(productId)
+
+    const isReviewed = product.reviews.find(
+        r => r.user.toString() === req.user._id.toString()
+    )
+
+        if(isReviewed) {
+            product.reviews.forEach(review => {
+                if(review.user.toString() === req.user._id.toString()) {
+                    review.comment = comment;
+                    review.rating = rating;
+                }
+            })
+
+        } else {
+            product.reviews.push(review);
+            product.numOfReviews = product.reviews.length
+        }
+
+        product.ratings =  product.reviews.reduce((acc, item) => item.rating + acc, 0) / product.
+        reviews.length
+
+        await product.save({ validateBeforeSave: false})
+
+        res.status(200).json({
+            success: true
+        })
+})
+
+exports.getProductReviews = catchAsyncErrors( async (req, res ,next) =>{
+    const product = await Product.findById(req.query.id);
+
+    res.status(200).json({
+        success: true,
+        reviews: product.reviews
+    })
+})
+
+exports.deleteReview = catchAsyncErrors( async (req, res ,next) => {
+    const product = await Product.findById(req.query.productId);
+
+    const reviews = product.reviews.filter(review => review._id.toString() !== req.query.id.
+    toString());
+
+    const numOfReviews = reviews.lenght;
+
+    const ratings = product.reviews.reduce((acc, item)=> item.rating + acc, 0) / reviews.lenght;
+
+    await Product.findByIdAndUpdate(req.query.productId, {
+        reviews,
+        ratings,
+        numOfReviews
+    }, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    })
+
+    res.status(200).json({
+        success: true
+    })
+})
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const router = express.Router();
 
-const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/productController')
+const { getProducts, 
+    newProduct, 
+    getSingleProduct, 
+    updateProduct, 
+    deleteProduct, 
+    createProductReview, 
+    getProductReviews,
+    deleteReview 
+} = require('../controllers/productController')
 
 
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth')
@@ -15,5 +23,10 @@ router.route('/admin/product/new').post(isAuthenticatedUser, authorizeRoles('adm
 router.route('/admin/product/:id').put(isAuthenticatedUser, authorizeRoles('admin'), updateProduct);
 
 router.route('/admin/product/:id').delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProduct);
+
+
+router.route('/review').put(isAuthenticatedUser, createProductReview)
+router.route('/reviews').get(isAuthenticatedUser, getProductReviews)
+router.route('/reviews').delete(isAuthenticatedUser, deleteReview)
 
 module.exports = router;
