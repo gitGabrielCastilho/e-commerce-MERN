63d97802243c63c8b7e1a0de25a1776839c2c495
Gabriel <gabrielmcastilho@gmail.com>
2022-10-18 11:44:56 -0300

    home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
 import Footer from "./component/layout/Footer";
 import Header from "./component/layout/Header";
+import Home from "./component/layout/Home";
 
 function App() {
   return (
+  <Router>  
   <div className="App">
     <Header/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
     <Footer/>
   </div>
+  </Router> 
   );
 }
 
